e2c376ac2dc4939e25118b689a7b778e2581eb8d
Kendall Strautman <kendallstrautman@gmail.com>
2020-03-23 15:01:06 -0600

    chore: interim show sidebar for globalstyles workaround

--- pages/_app.js
+++ pages/_app.js
@@ -1,15 +1,16 @@
 import React from 'react'
 import App from 'next/app'
-import { Tina, TinaCMS } from 'tinacms'
+import { TinaProvider, TinaCMS } from 'tinacms'
 import { GitClient, GitMediaStore } from '@tinacms/git-client'
 
 export default class Site extends App {
   constructor() {
     super()
+
     this.cms = new TinaCMS({
       sidebar: {
         position: 'overlay',
-        hidden: process.env.NODE_ENV === 'production',
+        hidden: false, // TODO: once GlobalStyles is fixed, hide this
       },
     })
     const client = new GitClient('http://localhost:3000/___tina')
@@ -20,9 +21,9 @@ export default class Site extends App {
   render() {
     const { Component, pageProps } = this.props
     return (
-      <Tina cms={this.cms}>
+      <TinaProvider cms={this.cms}>
         <Component {...pageProps} />
-      </Tina>
+      </TinaProvider>
     )
   }
 }
