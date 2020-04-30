273858c7f34c94c28fd23c78585c69f5e49e312b
Kendall Strautman <kendallstrautman@gmail.com>
2020-04-30 13:56:51 -0700

    feat: adds github source and preview mode to index

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,32 +1,46 @@
+import { getGithubPreviewProps, parseJson } from 'next-tinacms-github'
+
 import Layout from '../components/Layout'
 import IndexBlocks from '../components/IndexBlocks'
 
 const Index = props => {
+  console.log('props!', props)
   return (
     <Layout
-      pathname="/"
+      editMode={props.preview}
       siteTitle={props.title}
       siteDescription={props.description}
       infoBlurb={props.infoBlurb}
     >
-      <IndexBlocks form={form} />
+      {/* <IndexBlocks form={form} /> */}
     </Layout>
   )
 }
 
 export default Index
 
-export async function getStaticProps() {
-  const configData = await import(`../data/config.json`)
-  const blocksData = await import('../data/blocks.json')
+export async function getStaticProps<GetStaticProps>({ preview, previewData }) {
+  if (preview) {
+    return getGithubPreviewProps({
+      ...previewData,
+      fileRelativePath: '/data/blocks.json',
+      parse: parseJson,
+    })
+  } else {
+    const configData = await import(`../data/config.json`)
+    const blocksData = await import('../data/blocks.json')
 
-  return {
-    props: {
-      ...configData,
-      jsonFile: {
-        fileRelativePath: `data/blocks.json`,
-        data: blocksData.default,
+    return {
+      props: {
+        ...configData,
+        sourceProvider: null,
+        error: null,
+        preview: false,
+        jsonFile: {
+          fileRelativePath: `data/blocks.json`,
+          data: blocksData.default,
+        },
       },
-    },
+    }
   }
 }
