7f5cd3f0dc5d36df9cbdab0483c117ee9058f454
Kendall Strautman <kendallstrautman@gmail.com>
2020-05-04 16:16:52 -0700

    chore: remove webpack helpers

--- next.config.js
+++ next.config.js
@@ -1,6 +1,5 @@
 const withSvgr = require('next-svgr')
 require('dotenv').config()
-// const tinaWebpackHelpers = require('../../tinacms/packages/@tinacms/webpack-helpers')
 
 module.exports = withSvgr({
   env: {
@@ -10,12 +9,7 @@ module.exports = withSvgr({
     BASE_BRANCH: process.env.BASE_BRANCH,
   },
   exportTrailingSlash: true,
-  webpack: (config, { buildId, dev, isServer, defaultLoaders, webpack }) => {
-    // if (dev) {
-    //   tinaWebpackHelpers.aliasTinaDev(config, '../../tinacms', [
-    //     'react-tinacms-inline',
-    //   ])
-    // }
+  webpack: config => {
     config.node = {
       fs: 'empty',
     }
