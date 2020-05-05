1b498a4ede7b3828ae839423acb120bbbd765eb5
Kendall Strautman <kendallstrautman@gmail.com>
2020-05-05 08:50:06 -0700

    feat: adds github media store

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,22 +1,30 @@
 import React from 'react'
 import App from 'next/app'
 import { TinaProvider, TinaCMS } from 'tinacms'
-import { GithubClient, TinacmsGithubProvider } from 'react-tinacms-github'
+import {
+  GithubClient,
+  TinacmsGithubProvider,
+  GithubMediaStore,
+} from 'react-tinacms-github'
 
 export default class Site extends App {
   cms: TinaCMS
 
   constructor(props) {
     super(props)
+    const client = new GithubClient({
+      proxy: '/api/proxy-github',
+      authCallbackRoute: '/api/create-github-access-token',
+      clientId: process.env.GITHUB_CLIENT_ID,
+      baseRepoFullName: process.env.BASE_REPO_FULL_NAME, // e.g: tinacms/tinacms.org,
+    })
 
     this.cms = new TinaCMS({
       apis: {
-        github: new GithubClient({
-          proxy: '/api/proxy-github',
-          authCallbackRoute: '/api/create-github-access-token',
-          clientId: process.env.GITHUB_CLIENT_ID,
-          baseRepoFullName: process.env.BASE_REPO_FULL_NAME, // e.g: tinacms/tinacms.org,
-        }),
+        github: client,
+      },
+      media: {
+        store: new GithubMediaStore(client),
       },
       sidebar: {
         hidden: true,
