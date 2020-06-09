654e2d4a3f318c664aded69d0ba3895406500ed5
Kendall Strautman <kendallstrautman@gmail.com>
2020-06-09 15:01:18 -0700

    add comment on block index helper

--- utils/getBlockIndex.ts
+++ utils/getBlockIndex.ts
@@ -1,3 +1,8 @@
+/**
+ * Assumes the block naming convention doesn't change in Tina
+ * And will only work with non-nested blocks
+ *  */
+
 export function getBlockIndex({ name }: { name: string }): string {
   return name.split('.')[1]
 }
